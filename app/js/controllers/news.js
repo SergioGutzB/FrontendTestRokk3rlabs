4f81f56caa0a8757e7c4af45fca01bcabc2ddf80
Sergio Gutiérrez <sergut18@gmail.com>
2017-01-05 14:36:50 -0500

    add mansory grid news

--- app/js/controllers/news.js
+++ app/js/controllers/news.js
@@ -4,4 +4,31 @@ newsController.$inject = [];
 
 function newsController() {
   var self = this;
+
+  (function($) {
+    var $container = $('.news');
+    $container.imagesLoaded(function() {
+      $container.masonry({
+        columnWidth: '.new',
+        itemSelector: '.new'
+      });
+    });
+
+    //Reinitialize masonry inside each panel after the relative tab link is clicked - 
+    $('a[data-toggle=tab]').each(function() {
+      var $this = $(this);
+
+      $this.on('shown.bs.tab', function() {
+
+        $container.imagesLoaded(function() {
+          $container.masonry({
+            columnWidth: '.new',
+            itemSelector: '.news'
+          });
+        });
+
+      }); //end shown
+    }); //end each
+
+  })(jQuery);
 }
